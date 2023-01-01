7edb1ca6d32c5e3acbfe6beaff432237f2a8f02a
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-01-01 00:42:17 -0300

    Estrutura de Repetição
     Estrutura de Repetição

---
+++ exercicio03.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+    $in = $_GET["inicio"]?$_GET["inicio"]:0;
+    $fim = $_GET["fina"] ? $_GET["fina"] : 0;
+    $inc = $_GET["incremento"] ? $_GET["incremento"] : 0;
+    echo "$in";
+    if($in < $fim) /*Essa condição é para especificar que haverá uma incrementação */
+    {
+      while($in<=$fim)
+    {
+        $in += $inc;
+
+        if($in<=$fim)
+        {
+          echo "$in";
+        }
+    }
+    }
+    else /*Nesse caso,essa condição é para especificar que haverá uma decrementação*/
+    {
+      while($in >= $fim)
+      {
+        $in -= $inc;
+
+        if($in >= $fim)
+        {
+          echo "$in";
+        }
+      }
+    }
+    ?>
+</div>
+</body>
+</html>
+ 
